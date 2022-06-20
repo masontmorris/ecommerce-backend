5d1e2c4d8f1aadb362aef0b87bc9df9b293ff523
masontmorris <masonmorris8@gmail.com>
2022-06-19 22:26:52 -0400

    finished routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
         include: [
             {
                 model: Product,
-                attributes: ["product_name"],
+                attributes: ["product_name", "price", "stock"],
             },
         ],
     }).then((categories) => {
@@ -25,7 +25,7 @@ router.get("/:id", (req, res) => {
         include: [
             {
                 model: Product,
-                attributes: ["product_name"],
+                attributes: ["product_name", "price", "stock"],
             },
         ],
     }).then((category) => {
